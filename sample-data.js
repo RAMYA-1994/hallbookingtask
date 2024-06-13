676ccc6835ff2f30875552d829abbccd0a84a7c5
RAMYA-1994 <ramyarrr2016@gmail.com>
2024-06-13 12:42:24 +0530

    first commit

---
+++ sample-data.js
@@ -0,0 +1,32 @@
+let rooms = [{
+    roomId:"V1",
+    seatsAvailable:"4",
+    amenities:"tv,ac,heater",
+    pricePerhr:"100"
+}];
+let bookings = [{
+    customer: "Ramya",
+    bookingDate: "20240819",
+    startTime: "12:00pm",
+    endTime: "11:59am",
+    bookingID: "S1",
+    roomId: "V1",
+    status: "booked",
+    booked_On: "3/10/2024"
+}
+];
+let customers = [
+    { name: 'Ramya',
+     bookings: [ 
+        {
+            customer: 'Ramya',
+            bookingDate: '20240819',
+            startTime: '12:00pm',
+            endTime: '11:59am',
+            bookingID: 'S1',
+            roomId: 'V1',
+            status: 'booked',
+            booked_On: '3/10/2024'
+          }
+      ] }
+];
